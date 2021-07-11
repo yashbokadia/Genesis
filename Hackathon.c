7fcb94dafe924a9f80189e7b31ca508bc284a952
yashbokadia <84057253+yashbokadia@users.noreply.github.com>
2021-07-11 15:30:10 +0530

    Update Hackathon.c

--- Hackathon.c
+++ Hackathon.c
@@ -1,9 +1,28 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
+
+struct signupinfo
+    {
+    char fname[20];
+    char lname[20];
+    int mob[11];
+    char pass[50];
+    char cpass[50];
+    };
+
 void display();
 void working();
 void signin();
 void signup();
+void select();
+void pay();
+void checkbal();
+void mobilerecharge();
+void dthrecharge();
+void addrecipent();
+void settings();
+
 int main()
 {
     system("cls");                      //To clear the screen first time
@@ -22,11 +41,12 @@ int main()
         signup();
         break;
 
-        default:                      //If a wrong option is chooosen then it will be redirected to the same page
+        default:                      //If a wrong option is choosen then it will be redirected to the same page
         main();
     }
     return 0;
 }
+
 void signin()
 {
     int key;
@@ -34,6 +54,7 @@ void signin()
     do{                               //do while loop because it can create a similar effect as a swipeable menu
         system("cls");
         display(count);
+        printf("\n\nEnter 77 to go to next page\nEnter 75 to go to previous page\nEnter 10 to select current page\nEnter 8 to go to Main Menu\n\n");
         fflush(stdin);
         scanf("%d",&key);
         if(key==77)   //right arrow key=77
@@ -55,7 +76,9 @@ void signin()
         if(key==8)  //Backspace key    for going back to main menu
         main();
     }while(key!=10);   //enter key=10   for selecting option
+    select(count);
 }
+
 void display(int count)
 {
     if(count==0)
@@ -76,29 +99,31 @@ void display(int count)
         else if(count==5)
     printf("Settings Page\n");
 }
+
 void signup()
 {
     //system("cls");
-    char fname[20];
-    char lname[20];
-    int mob[11];
-    char pass[50];
-    char cpass[50];
-    int checkpoint;
+    //char fname[20];
+    //char lname[20];
+    //int mob[11];
+    //char pass[50];
+    //char cpass[50];
+    int checkpoint,i=0;
+    struct signupinfo customer[100];
     printf("Enter Your First Name    : ");
-    scanf("%s",&fname);
+    scanf("%s",&customer[i].fname);
     printf("Enter Your Last Name     : ");
-    scanf("%s",&lname);
+    scanf("%s",&customer[i].lname);
     printf("Enter Your Mobile Number : ");
     fflush(stdin);
-    scanf("%s",&mob);
+    scanf("%s",&customer[i].mob);
     printf("Set Password             : ");
     fflush(stdin);
-    scanf("%s",&pass);
+    scanf("%s",&customer[i].pass);
     printf("Confirm Password         : ");
     fflush(stdin);
-    scanf("%s",&cpass);
-    checkpoint=strcmp(cpass,pass);          //comparison of password
+    scanf("%s",&customer[i].cpass);
+    checkpoint=strcmp(customer[i].cpass,customer[i].pass);          //comparison of password
     if(checkpoint==0)                       //condition of strcmp function i.e. if it return 0 then the strings(password) are same
     signin();
     else
@@ -107,4 +132,62 @@ void signup()
         printf("Please signup again\n\n");
         signup();
     }
-}   //10 july 2021
+}
+
+void select(int count)
+{
+        if(count==0)
+        pay();
+    
+        else if(count==1)
+        checkbal();
+
+        else if(count==2)
+        mobilerecharge();
+
+        else if(count==3)
+        dthrecharge();
+
+        else if(count==4)
+        addrecipent();
+
+        else if(count==5)
+        settings();    
+}
+
+void pay()
+{
+    system("cls");
+    printf("You are in pay page\n");
+    
+}
+
+void checkbal()
+{
+    system("cls");
+    printf("You are in check balance page\n");
+}
+
+void mobilerecharge()
+{
+    system("cls");
+    printf("You are in mobile recharge page\n");
+}
+
+void dthrecharge()
+{
+    system("cls");
+    printf("You are in dth recharge page\n");
+}
+
+void addrecipent()
+{
+    system("cls");
+    printf("You are in add recipent page\n");
+}
+
+void settings()
+{
+    system("cls");
+    printf("You are in settings page\n");
+}
